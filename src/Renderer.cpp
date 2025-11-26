71ed3aac7a5b8544ec5720cfccec54a58304423d
Dave Hillier <dave.hillier1@gmail.com>
2025-11-26 19:07:49 +0000

    Add terrain shader files and refactor terrain configuration structure

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -160,7 +160,7 @@ bool Renderer::init(SDL_Window* win, const std::string& resPath) {
     terrainInfo.texturePath = resourcePath + "/textures";
     terrainInfo.framesInFlight = MAX_FRAMES_IN_FLIGHT;
 
-    TerrainSystem::TerrainConfig terrainConfig{};
+    TerrainConfig terrainConfig{};
     terrainConfig.size = 500.0f;
     terrainConfig.heightScale = 50.0f;
     terrainConfig.maxDepth = 18;  // Reasonable depth for testing
